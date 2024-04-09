9a3c1ea3e3e12b9a138f1d4af7bc53396c7c0fc6
Monica Hanson <mhanson@monicarose.tech>
2024-04-09 09:19:21 -0500

    Initial rewrite

---
+++ main.go
@@ -0,0 +1,433 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"go.bug.st/serial"
+	"go.bug.st/serial/enumerator"
+	"log"
+	"os"
+	"os/signal"
+	"runtime"
+	"strconv"
+	"strings"
+	"time"
+)
+
+func ReadLine(port serial.Port, buffSize int, debug bool) []byte {
+	line := ReadLines(port, buffSize, 1, debug)
+	return line[0]
+}
+
+func ReadLines(port serial.Port, buffSize int, maxLines int, debug bool) [][]byte {
+	noDataCount := 0
+	lineCount := 0
+	output := make([][]byte, maxLines)
+	for i := 0; i < maxLines; i++ {
+		output[i] = make([]byte, buffSize)
+		for {
+			// Reads up to buffSize bytes
+			n, err := port.Read(output[i])
+			if err != nil {
+				log.Fatal(err)
+			}
+			if n == 0 {
+				noDataCount += 1
+			} else {
+				noDataCount = 0
+			}
+			if n == '\n' {
+				lineCount++
+			}
+			if noDataCount >= 300 {
+				break
+			} else if lineCount >= maxLines {
+				break
+			}
+
+			fmt.Printf("%s", string(output[i][:n]))
+		}
+		if debug {
+			fmt.Printf("DEBUG: %s", output[i])
+		}
+	}
+	return output
+}
+
+func WaitForPrefix(port serial.Port, prompt string, debug bool) {
+	var output []byte
+	if debug {
+		for !strings.HasPrefix(strings.ToLower(strings.TrimSpace(string(output[:]))), prompt) {
+			fmt.Printf("Has prefix: %t\n", strings.HasPrefix(strings.ToLower(strings.TrimSpace(string(output[:]))), prompt))
+			fmt.Printf("Expected prefix: %s\n", prompt)
+			port.Write(FormatCommand(""))
+			output = ReadLine(port, 32768, debug)
+			fmt.Printf("DEBUG: %s", output)
+		}
+		fmt.Println(output)
+	} else {
+		for !strings.HasPrefix(strings.ToLower(strings.TrimSpace(string(output[:]))), prompt) {
+			fmt.Printf("Has prefix: %t\n", strings.HasPrefix(strings.ToLower(strings.TrimSpace(string(output[:]))), prompt))
+			fmt.Printf("Expected prefix: %s\n", prompt)
+			port.Write(FormatCommand(""))
+			output = ReadLine(port, 32768, debug)
+		}
+	}
+}
+
+func WaitForSubstring(port serial.Port, prompt string, debug bool) {
+	var output []byte
+	if debug {
+		for !strings.Contains(strings.ToLower(strings.TrimSpace(string(output[:]))), prompt) {
+			fmt.Printf("Has prefix: %t\n", strings.Contains(strings.ToLower(strings.TrimSpace(string(output[:]))), prompt))
+			fmt.Printf("Expected prefix: %s\n", prompt)
+			fmt.Printf("DEBUG: %s", output)
+			port.Write(FormatCommand(""))
+			output = ReadLine(port, 32768, debug)
+		}
+		fmt.Println(output)
+	} else {
+		for !strings.Contains(strings.ToLower(strings.TrimSpace(string(output[:]))), prompt) {
+			fmt.Printf("Has prefix: %t\n", strings.Contains(strings.ToLower(strings.TrimSpace(string(output[:]))), prompt))
+			fmt.Printf("Expected prefix: %s\n", prompt)
+			port.Write(FormatCommand(""))
+			output = ReadLine(port, 32768, debug)
+		}
+	}
+}
+
+func FormatCommand(cmd string) []byte {
+	formattedString := []byte(cmd + "\n")
+	return formattedString
+}
+
+func ParseFilesToDelete(files [][]byte, debug bool) []string {
+	commonPrefixes := []string{"private-config", "config", "vlan"}
+	filesToDelete := make([]string, len(files))
+
+	if debug {
+		for _, file := range files {
+			cleanLine := strings.Split(strings.TrimSpace(string(file)), " ")
+			for _, prefix := range commonPrefixes {
+				if strings.HasPrefix(strings.ToLower(strings.TrimSpace(cleanLine[len(cleanLine)-1])), prefix) {
+					filesToDelete[len(filesToDelete)] = cleanLine[len(cleanLine)-1]
+				}
+			}
+		}
+	} else {
+		for _, file := range files {
+			cleanLine := strings.Split(strings.TrimSpace(string(file)), " ")
+			for _, prefix := range commonPrefixes {
+				if strings.HasPrefix(strings.ToLower(strings.TrimSpace(cleanLine[len(cleanLine)-1])), prefix) {
+					filesToDelete[len(filesToDelete)] = cleanLine[len(cleanLine)-1]
+				}
+			}
+		}
+	}
+
+	return filesToDelete
+}
+
+func SetupSerial() string {
+	var userInput string
+	isValid := false
+	for !isValid {
+		ports, err := enumerator.GetDetailedPortsList()
+		if err != nil {
+			log.Fatal(err)
+		}
+		if len(ports) == 0 {
+			log.Fatal("No serial ports found!")
+		}
+		for _, port := range ports {
+			fmt.Printf("Found port %v\n", port.Name)
+			fmt.Printf("\tDescription:\t%s\n", port.Product)
+			if port.IsUSB {
+				fmt.Printf("\tUSB ID\t\t%s:%s\n", port.VID, port.PID)
+				fmt.Printf("\tUSB Serial\t%s\n", port.SerialNumber)
+			}
+		}
+
+		fmt.Printf("Select a serial port ")
+		fmt.Scanln(&userInput)
+
+		for _, port := range ports {
+			if strings.ToUpper(userInput) == strings.ToUpper(port.Name) {
+				isValid = true
+			}
+		}
+	}
+	return userInput
+}
+
+func RouterDefaults(SerialPort string, debug bool) {
+	const BUFFER_SIZE = 32768
+	const SHELL_PROMPT = "router"
+	const ROMMON_PROMPT = "rommon"
+	const CONFIRMATION_PROMPT = "[confirm]"
+	const RECOVERY_REGISTER = "0x2142"
+	const NORMAL_REGISTER = "0x2102"
+	const SAVE_PROMPT = "[yes/no]: "
+
+	mode := &serial.Mode{
+		BaudRate: 9600,
+		Parity:   serial.NoParity,
+		DataBits: 8,
+		StopBits: serial.OneStopBit,
+	}
+
+	port, err := serial.Open(SerialPort, mode)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	port.SetReadTimeout(1)
+
+	fmt.Println("Trigger the recovery sequence by following these steps: ")
+	fmt.Println("1. Turn off the router")
+	fmt.Println("2. After waiting for the lights to shut off, turn the router back on")
+	fmt.Println("3. Press enter here once this has been completed")
+	fmt.Scanln()
+
+	fmt.Println("Sending ^C until we get into ROMMON...")
+	var output []byte
+
+	// Get to ROMMON
+	if debug {
+		for !strings.HasPrefix(strings.ToLower(strings.TrimSpace(string(output[:]))), ROMMON_PROMPT) {
+			fmt.Printf("Has prefix: %t\n", strings.HasPrefix(strings.ToLower(strings.TrimSpace(string(output[:]))), ROMMON_PROMPT))
+			fmt.Printf("Expected prefix: %s\n", ROMMON_PROMPT)
+			output = ReadLine(port, BUFFER_SIZE, debug)
+			time.Sleep(1 * time.Second)
+			fmt.Printf("DEBUG: %s", strings.ToLower(strings.TrimSpace(string(output[:]))))
+			port.Write([]byte("\x03"))
+		}
+		fmt.Println(output)
+	} else {
+		for !strings.HasPrefix(strings.ToLower(strings.TrimSpace(string(output[:]))), ROMMON_PROMPT) {
+			fmt.Printf("Has prefix: %t\n", strings.HasPrefix(strings.ToLower(strings.TrimSpace(string(output[:]))), ROMMON_PROMPT))
+			fmt.Printf("Expected prefix: %s\n", ROMMON_PROMPT)
+			port.Write([]byte("\x03"))
+			output = ReadLine(port, BUFFER_SIZE, debug)
+			time.Sleep(1 * time.Second)
+		}
+	}
+
+	// In ROMMON
+	fmt.Println("We've entered ROMMON, setting the register to 0x2142.")
+	commands := []string{"confreg " + RECOVERY_REGISTER, "reset"}
+
+	for idx, cmd := range commands {
+		WaitForPrefix(port, ROMMON_PROMPT+" "+strconv.Itoa(idx+1), debug)
+		port.Write(FormatCommand(cmd))
+		output = ReadLine(port, BUFFER_SIZE, debug)
+		if debug {
+			fmt.Printf("DEBUG: %s", output)
+		}
+
+	}
+	for strings.HasPrefix(strings.ToLower(strings.TrimSpace(string(output[:]))), ROMMON_PROMPT) {
+		port.Write(FormatCommand(""))
+		if debug {
+			fmt.Printf("DEBUG: %s", output)
+		}
+		time.Sleep(1 * time.Second)
+	}
+
+	// We've made it out of ROMMON
+	// Set timeout (does this do anything? idk)
+	port.SetReadTimeout(15)
+	fmt.Println("We've finished with ROMMON, going back into the regular console")
+	for !strings.HasPrefix(strings.ToLower(strings.TrimSpace(string(output[:]))), ROMMON_PROMPT) {
+		if debug {
+			fmt.Printf("DEBUG: %s", output)
+		}
+		output = ReadLine(port, BUFFER_SIZE, debug)
+		fmt.Printf("DEBUG: Output size: %d\n", len(strings.TrimSpace(string(output))))
+		if len(strings.TrimSpace(string(output))) == 0 {
+			port.Write([]byte("\r\n"))
+		}
+		time.Sleep(1 * time.Second)
+	}
+
+	fmt.Println("Setting the registers back to regular")
+	port.SetReadTimeout(1)
+	// We can safely assume we're at the prompt, begin running reset commands
+	commands = []string{"enable", "conf t", "config-register " + NORMAL_REGISTER, "end"}
+	for _, cmd := range commands {
+		port.Write(FormatCommand(cmd))
+		ReadLines(port, BUFFER_SIZE, 2, debug)
+
+		// In case commands want to give some flavor text, just disregard those
+		WaitForPrefix(port, SHELL_PROMPT, debug)
+	}
+
+	// Now reset config and restart
+	fmt.Println("Resetting the configuration")
+	port.Write(FormatCommand("erase nvram:"))
+	ReadLines(port, BUFFER_SIZE, 2, debug)
+	port.Write(FormatCommand(""))
+	ReadLines(port, BUFFER_SIZE, 2, debug)
+
+	WaitForPrefix(port, SHELL_PROMPT, debug)
+	fmt.Println("Reloading the router")
+	port.Write(FormatCommand("reload"))
+	WaitForSubstring(port, SAVE_PROMPT, debug)
+	port.Write(FormatCommand("yes"))
+	WaitForSubstring(port, SAVE_PROMPT, debug)
+
+	fmt.Println("Successfully reset! Will continue trailing the output, but ^C at any point to exit.")
+	PrintOutput(port)
+}
+
+func SwitchDefaults(SerialPort string, debug bool) {
+	const BUFFER_SIZE = 32768
+	const RECOVERY_PROMPT = "switch:"
+	const CONFIRMATION_PROMPT = "[confirm]"
+	const STARTUP_HINT = "Xmodem file system is available."
+
+	mode := &serial.Mode{
+		BaudRate: 9600,
+		Parity:   serial.NoParity,
+		DataBits: 8,
+		StopBits: serial.OneStopBit,
+	}
+
+	port, err := serial.Open(SerialPort, mode)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	port.SetReadTimeout(1)
+
+	fmt.Println("Trigger password recovery by following these steps: ")
+	fmt.Println("1. Unplug the switch")
+	fmt.Println("2. Hold the MODE button on the switch.")
+	fmt.Println("3. Plug the switch in while holding the button")
+	fmt.Println("4. When you are told, release the MODE button")
+	fmt.Println("Press Enter here when you have plugged the switch back in")
+	fmt.Scanln()
+
+	// Wait for switch to startup
+	WaitForPrefix(port, STARTUP_HINT, debug)
+	fmt.Println("Release the MODE button now")
+	WaitForPrefix(port, RECOVERY_PROMPT, debug)
+
+	// Initialize Flash
+	fmt.Println("Entered recovery console, now initializing flash")
+	port.Write(FormatCommand("flash_init"))
+	WaitForPrefix(port, RECOVERY_PROMPT, debug)
+
+	// Get files
+	fmt.Println("Flash has been initialized, now listing directory")
+	port.SetReadTimeout(15)
+	listing := ReadLines(port, BUFFER_SIZE, 30, debug)
+	WaitForPrefix(port, RECOVERY_PROMPT, debug)
+
+	// Determine the files we need to delete
+	fmt.Println("Parsing files to delete...")
+	filesToDelete := ParseFilesToDelete(listing, debug)
+
+	// Delete files if necessary
+	if len(filesToDelete) == 0 {
+		fmt.Println("Switch has been reset already.")
+	} else {
+		fmt.Println("Deleting files")
+		for _, file := range filesToDelete {
+			fmt.Println("Deleting " + file)
+			port.Write(FormatCommand("del flash:" + file))
+			WaitForSubstring(port, CONFIRMATION_PROMPT, debug)
+			if debug {
+				fmt.Printf("DEBUG: Confirming deletion\n")
+			}
+			port.Write(FormatCommand("y"))
+			ReadLines(port, BUFFER_SIZE, 2, debug)
+		}
+		fmt.Println("Switch has been reset")
+	}
+
+	fmt.Println("Restarting the switch")
+	WaitForPrefix(port, RECOVERY_PROMPT, debug)
+
+	port.Write(FormatCommand("reset"))
+	WaitForSubstring(port, CONFIRMATION_PROMPT, debug)
+
+	port.Write(FormatCommand("y"))
+	ReadLines(port, BUFFER_SIZE, 10, debug)
+
+	fmt.Println("Successfully reset! Will continue trailing the output, but ^C at any point to exit.")
+	PrintOutput(port)
+}
+
+func PrintOutput(port serial.Port) {
+	c := make(chan os.Signal)
+	signal.Notify(c, os.Interrupt)
+
+	go func() {
+		<-c
+		port.Close()
+		return
+	}()
+	for true {
+		ReadLine(port, 32768, true)
+	}
+}
+
+func TrailOutput(SerialPort string) {
+	mode := &serial.Mode{
+		BaudRate: 9600,
+	}
+
+	port, err := serial.Open(SerialPort, mode)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	port.SetReadTimeout(10)
+
+	for {
+		scanner := bufio.NewScanner(os.Stdin)
+		scanner.Scan()
+		userInput := scanner.Text()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		_, err = port.Write(FormatCommand(userInput))
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		ReadLines(port, 32768, 2, true)
+	}
+}
+
+func main() {
+	var debug bool
+	var resetRouter bool
+	var resetSwitch bool
+	var serialDevice string
+
+	flag.BoolVar(&debug, "debug", false, "Show debugging messages")
+	flag.BoolVar(&resetRouter, "router", false, "Reset a router")
+	flag.BoolVar(&resetSwitch, "switch", false, "Reset a switch")
+	flag.Parse()
+
+	fmt.Printf("The application was built with the Go version: %s\n", runtime.Version())
+
+	if resetRouter || resetSwitch {
+		serialDevice = SetupSerial()
+	} else {
+		log.Fatal("Neither router or switch reset flags provided. Run program with -router and/or -switch")
+	}
+
+	if resetRouter {
+		RouterDefaults(serialDevice, debug)
+	}
+	if resetSwitch {
+		SwitchDefaults(serialDevice, debug)
+	}
+}
