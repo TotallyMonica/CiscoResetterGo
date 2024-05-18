997e93303c73be7a1cf5261340ccd659abb9cdb4
Monica Hanson <mhanson@monicarose.tech>
2024-05-18 15:24:05 -0500

    More refactoring

--- common/common.go
+++ common/common.go
@@ -4,7 +4,35 @@ import (
 	"fmt"
 	"go.bug.st/serial"
 	"log"
+	"strings"
+	"time"
 )
+
+func WaitForSubstring(port serial.Port, prompt string, debug bool) {
+	var output []byte
+	if debug {
+		for !strings.Contains(strings.ToLower(strings.TrimSpace(string(output[:]))), prompt) {
+			fmt.Printf("Has prefix: %t\n", strings.Contains(strings.ToLower(strings.TrimSpace(string(output[:]))), prompt))
+			fmt.Printf("Expected substring: %s\n", prompt)
+			fmt.Printf("FROM DEVICE: %s", strings.TrimSpace(string(output)))
+			fmt.Printf("TO DEVICE: %s\n", "\\n")
+			port.Write(FormatCommand(""))
+			output = TrimNull(ReadLine(port, 500, debug))
+			time.Sleep(1 * time.Second)
+
+		}
+		fmt.Println(output)
+	} else {
+		for !strings.Contains(strings.ToLower(strings.TrimSpace(string(TrimNull(output[:])))), prompt) {
+			fmt.Printf("Has prefix: %t\n", strings.Contains(strings.ToLower(strings.TrimSpace(string(output[:]))), prompt))
+			fmt.Printf("Expected substring: %s\n", prompt)
+			port.Write(FormatCommand(""))
+			output = TrimNull(ReadLine(port, 500, debug))
+			time.Sleep(1 * time.Second)
+
+		}
+	}
+}
 
 func FormatCommand(cmd string) []byte {
 	formattedString := []byte(cmd + "\n")
--- main.go
+++ main.go
@@ -40,32 +40,6 @@ func WaitForPrefix(port serial.Port, prompt string, debug bool) {
 	}
 }
 
-func WaitForSubstring(port serial.Port, prompt string, debug bool) {
-	var output []byte
-	if debug {
-		for !strings.Contains(strings.ToLower(strings.TrimSpace(string(output[:]))), prompt) {
-			fmt.Printf("Has prefix: %t\n", strings.Contains(strings.ToLower(strings.TrimSpace(string(output[:]))), prompt))
-			fmt.Printf("Expected substring: %s\n", prompt)
-			fmt.Printf("FROM DEVICE: %s", strings.TrimSpace(string(output)))
-			fmt.Printf("TO DEVICE: %s\n", "\\n")
-			port.Write(common.FormatCommand(""))
-			output = common.TrimNull(common.ReadLine(port, 4096, debug))
-			time.Sleep(1 * time.Second)
-
-		}
-		fmt.Println(output)
-	} else {
-		for !strings.Contains(strings.ToLower(strings.TrimSpace(string(common.TrimNull(output[:])))), prompt) {
-			fmt.Printf("Has prefix: %t\n", strings.Contains(strings.ToLower(strings.TrimSpace(string(output[:]))), prompt))
-			fmt.Printf("Expected substring: %s\n", prompt)
-			port.Write(common.FormatCommand(""))
-			output = common.TrimNull(common.ReadLine(port, 4096, debug))
-			time.Sleep(1 * time.Second)
-
-		}
-	}
-}
-
 func TrimNewLines(unparsed string) string {
 	friendlyLine := ""
 	for _, val := range unparsed {
