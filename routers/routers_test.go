499a55df516946eb729268a2c86b84835312597f
Monica Hanson <mrhanson1s@semo.edu>
2024-11-11 19:51:45 -0600

    More skip conditions, clean up new lines

--- routers/routers_test.go
+++ routers/routers_test.go
@@ -19,8 +19,11 @@ func validateOutput() {
 
 func TestReset(t *testing.T) {
 	if os.Getenv("CI") != "" {
-		t.Skip("Skipping test in CI environment\n")
+		t.Skip("Skipping test in CI environment")
+	} else if os.Getenv("SKIP_RESET_TESTS") != "" {
+		t.Skip("Skipping all reset tests")
 	}
+
 	type args struct {
 		SerialPort   string
 		PortSettings serial.Mode
@@ -28,32 +31,49 @@ func TestReset(t *testing.T) {
 		debug        bool
 		progressDest chan string
 	}
-	tests := []struct {
+
+	tests := make([]struct {
 		name string
 		args args
-	}{{
-		name: "Reset with verbose output",
-		args: args{
-			SerialPort:   "COM3",
-			PortSettings: serial.Mode{BaudRate: 9600, DataBits: 8, Parity: serial.NoParity, StopBits: serial.OneStopBit},
-			backup: common.Backup{
-				Backup: false,
+	}, 0)
+	if os.Getenv("SKIP_VERBOSE_RESET") != "" {
+		t.Skip("Skipping verbose output reset tests")
+	} else {
+		tests = append(tests, struct {
+			name string
+			args args
+		}{
+			name: "Reset with verbose output",
+			args: args{
+				SerialPort:   "COM3",
+				PortSettings: serial.Mode{BaudRate: 9600, DataBits: 8, Parity: serial.NoParity, StopBits: serial.OneStopBit},
+				backup: common.Backup{
+					Backup: false,
+				},
+				debug:        true,
+				progressDest: make(chan string),
 			},
-			debug:        true,
-			progressDest: make(chan string),
-		},
-	}, {
-		name: "Reset without verbose output",
-		args: args{
-			SerialPort:   "COM3",
-			PortSettings: serial.Mode{BaudRate: 9600, DataBits: 8, Parity: serial.NoParity, StopBits: serial.OneStopBit},
-			backup: common.Backup{
-				Backup: false,
+		})
+	}
+
+	if os.Getenv("SKIP_LIMITED_RESET") != "" {
+		t.Skip("Skipping limited output reset tests")
+	} else {
+		tests = append(tests, struct {
+			name string
+			args args
+		}{
+			name: "Reset without verbose output",
+			args: args{
+				SerialPort:   "COM3",
+				PortSettings: serial.Mode{BaudRate: 9600, DataBits: 8, Parity: serial.NoParity, StopBits: serial.OneStopBit},
+				backup: common.Backup{
+					Backup: false,
+				},
+				debug:        false,
+				progressDest: make(chan string),
 			},
-			debug:        false,
-			progressDest: make(chan string),
-		},
-	},
+		})
 	}
 	for _, tt := range tests {
 		start := time.Now()
@@ -67,25 +87,29 @@ func TestReset(t *testing.T) {
 			select {
 			case msg := <-tt.args.progressDest:
 				if strings.Contains(msg, "--EOF--") {
-					t.Logf("Test %s passed in %d:%d\n", tt.name, int(math.Floor(time.Since(start).Minutes())), int(math.Floor(time.Since(start).Seconds()))%60)
+					t.Logf("Test %s passed in %d:%d", tt.name, int(math.Floor(time.Since(start).Minutes())), int(math.Floor(time.Since(start).Seconds()))%60)
 					canExit = true
 				} else {
-					t.Logf("Test %s: %s\n", tt.name, msg)
+					t.Logf("Test %s: %s", tt.name, msg)
 				}
 			case <-timeout:
-				t.Fatalf("Test %s timed out in 20 minutes.\n", tt.name)
+				t.Fatalf("Test %s timed out in 20 minutes.", tt.name)
 			}
 			if canExit {
 				break
 			}
 		}
+		time.Sleep(5 * time.Second)
 	}
 }
 
 func TestDefaults(t *testing.T) {
 	if os.Getenv("CI") != "" {
-		t.Skip("Skipping test in CI environment\n")
+		t.Skip("Skipping test in CI environment")
+	} else if os.Getenv("SKIP_DEFAULTS") != "" {
+		t.Skip("Skipping all defaults tests")
 	}
+
 	type args struct {
 		SerialPort   string
 		PortSettings serial.Mode
@@ -96,44 +120,68 @@ func TestDefaults(t *testing.T) {
 
 	defaultsFile, err := os.OpenFile("router_defaults.json", os.O_RDONLY, 0666)
 	if err != nil {
-		t.Fatalf("Error while loading defaults file for testing: %s\n", err)
+		t.Fatalf("Error while loading defaults file for testing: %s", err)
 	}
 
-	defer defaultsFile.Close()
+	defer func(defaultsFile *os.File) {
+		err = defaultsFile.Close()
+		if err != nil {
+			t.Fatalf("Error while closing defaults file for testing: %s", err)
+		}
+	}(defaultsFile)
 
 	defaults, err := io.ReadAll(defaultsFile)
 	if err != nil {
-		t.Fatalf("Error while reading defaults file for testing: %s\n", err)
+		t.Fatalf("Error while reading defaults file for testing: %s", err)
 	}
 
 	defaultsStruct := RouterDefaults{}
 
 	err = json.Unmarshal(defaults, &defaultsStruct)
 	if err != nil {
-		t.Fatalf("Error while parsing defaults file for testing: %s\n", err)
+		t.Fatalf("Error while parsing defaults file for testing: %s", err)
 	}
 
-	tests := []struct {
+	tests := make([]struct {
 		name string
 		args args
-	}{{
-		name: "Apply defaults with verbose output",
-		args: args{
-			SerialPort:   "COM3",
-			PortSettings: serial.Mode{BaudRate: 9600, DataBits: 8, Parity: serial.NoParity, StopBits: serial.OneStopBit},
-			config:       defaultsStruct,
-			debug:        true,
-			progressDest: make(chan string),
-		},
-	}, {
-		name: "Apply defaults with limited output",
-		args: args{
-			SerialPort:   "COM3",
-			PortSettings: serial.Mode{BaudRate: 9600, DataBits: 8, Parity: serial.NoParity, StopBits: serial.OneStopBit},
-			config:       defaultsStruct,
-			progressDest: make(chan string),
-		},
-	}}
+	}, 0)
+
+	if os.Getenv("SKIP_VERBOSE_DEFAULTS") != "" {
+		t.Skip("Skipping verbose defaults tests")
+	} else {
+		tests = append(tests, struct {
+			name string
+			args args
+		}{
+			name: "Apply defaults with verbose output",
+			args: args{
+				SerialPort:   "COM3",
+				PortSettings: serial.Mode{BaudRate: 9600, DataBits: 8, Parity: serial.NoParity, StopBits: serial.OneStopBit},
+				config:       defaultsStruct,
+				debug:        true,
+				progressDest: make(chan string),
+			},
+		})
+	}
+
+	if os.Getenv("SKIP_LIMITED_DEFAULTS") != "" {
+		t.Skip("Skipping limited output defaults tests")
+	} else {
+		tests = append(tests, struct {
+			name string
+			args args
+		}{
+			name: "Apply defaults with limited output",
+			args: args{
+				SerialPort:   "COM3",
+				PortSettings: serial.Mode{BaudRate: 9600, DataBits: 8, Parity: serial.NoParity, StopBits: serial.OneStopBit},
+				config:       defaultsStruct,
+				progressDest: make(chan string),
+			},
+		})
+	}
+
 	for _, tt := range tests {
 		start := time.Now()
 		timeout := time.After(20 * time.Minute)
@@ -141,22 +189,32 @@ func TestDefaults(t *testing.T) {
 		go t.Run(tt.name, func(t *testing.T) {
 			Defaults(tt.args.SerialPort, tt.args.PortSettings, tt.args.config, tt.args.debug, tt.args.progressDest)
 		})
-		select {
-		case msg := <-tt.args.progressDest:
-			if strings.Contains(msg, "--EOF--") {
-				t.Logf("Test %s passed in %d:%d\n", tt.name, int(math.Floor(time.Since(start).Minutes())), int(math.Floor(time.Since(start).Seconds()))%60)
-			} else {
-				t.Logf("Test %s: %s\n", tt.name, msg)
+		for {
+			canExit := false
+			select {
+			case msg := <-tt.args.progressDest:
+				if strings.Contains(msg, "--EOF--") {
+					t.Logf("Test %s passed in %d:%d", tt.name, int(math.Floor(time.Since(start).Minutes())), int(math.Floor(time.Since(start).Seconds()))%60)
+					canExit = true
+				} else {
+					t.Logf("Test %s: %s", tt.name, msg)
+				}
+			case <-timeout:
+				t.Fatalf("Test %s timed out in 20 minutes.", tt.name)
 			}
-		case <-timeout:
-			t.Fatalf("Test %s timed out in 20 minutes.\n", tt.name)
+
+			if canExit {
+				break
+			}
 		}
+
+		time.Sleep(5 * time.Second)
 	}
 }
 
 func TestResetAndDefaults(t *testing.T) {
 	if os.Getenv("CI") != "" {
-		t.Skip("Skipping test in CI environment\n")
+		t.Skip("Skipping test in CI environment")
 	}
 
 	type resetArgs struct {
@@ -176,21 +234,21 @@ func TestResetAndDefaults(t *testing.T) {
 
 	defaultsFile, err := os.OpenFile("router_defaults.json", os.O_RDONLY, 0666)
 	if err != nil {
-		t.Fatalf("Error while loading defaults file for testing: %s\n", err)
+		t.Fatalf("Error while loading defaults file for testing: %s", err)
 	}
 
 	defer defaultsFile.Close()
 
 	defaults, err := io.ReadAll(defaultsFile)
 	if err != nil {
-		t.Fatalf("Error while reading defaults file for testing: %s\n", err)
+		t.Fatalf("Error while reading defaults file for testing: %s", err)
 	}
 
 	defaultsStruct := RouterDefaults{}
 
 	err = json.Unmarshal(defaults, &defaultsStruct)
 	if err != nil {
-		t.Fatalf("Error while parsing defaults file for testing: %s\n", err)
+		t.Fatalf("Error while parsing defaults file for testing: %s", err)
 	}
 
 	tests := []struct {
@@ -247,13 +305,13 @@ func TestResetAndDefaults(t *testing.T) {
 			select {
 			case msg := <-tt.resetArgs.progressDest:
 				if strings.Contains(msg, "--EOF--") {
-					t.Logf("Test %s passed in %d:%d\n", tt.name, int(math.Floor(time.Since(start).Minutes())), int(math.Floor(time.Since(start).Seconds()))%60)
+					t.Logf("Test %s passed in %d:%d", tt.name, int(math.Floor(time.Since(start).Minutes())), int(math.Floor(time.Since(start).Seconds()))%60)
 					canExit = true
 				} else {
-					t.Logf("Test %s: %s\n", tt.name, msg)
+					t.Logf("Test %s: %s", tt.name, msg)
 				}
 			case <-timeout:
-				t.Fatalf("Test %s timed out in 20 minutes.\n", tt.name)
+				t.Fatalf("Test %s timed out in 20 minutes.", tt.name)
 			}
 			if canExit {
 				break
@@ -266,12 +324,13 @@ func TestResetAndDefaults(t *testing.T) {
 		select {
 		case msg := <-tt.defaultsArgs.progressDest:
 			if strings.Contains(msg, "--EOF--") {
-				t.Logf("Test %s passed in %d:%d\n", tt.name, int(math.Floor(time.Since(start).Minutes())), int(math.Floor(time.Since(start).Seconds()))%60)
+				t.Logf("Test %s passed in %d:%d", tt.name, int(math.Floor(time.Since(start).Minutes())), int(math.Floor(time.Since(start).Seconds()))%60)
 			} else {
-				t.Logf("Test %s: %s\n", tt.name, msg)
+				t.Logf("Test %s: %s", tt.name, msg)
 			}
 		case <-timeout:
-			t.Fatalf("Test %s timed out in 20 minutes.\n", tt.name)
+			t.Fatalf("Test %s timed out in 20 minutes.", tt.name)
 		}
+		time.Sleep(5 * time.Second)
 	}
 }
