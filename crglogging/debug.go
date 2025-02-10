ad3e6d0c5d73d0fbc919445e8fd07a5f8f42f120
mhanson <mrhanson1s@semo.edu>
2025-02-10 16:39:30 -0600

    Logging should be using pointers

--- crglogging/debug.go
+++ crglogging/debug.go
@@ -4,17 +4,17 @@ type Debugf func(string, ...interface{})
 type Debugln func(string)
 type Debug func(string)
 
-func (l Crglogging) Debugf(format string, args ...interface{}) {
+func (l *Crglogging) Debugf(format string, args ...interface{}) {
 	l.logger.Debugf(format, args...)
 	l.DebugCount += 1
 }
 
-func (l Crglogging) Debugln(format ...interface{}) {
+func (l *Crglogging) Debugln(format ...interface{}) {
 	l.logger.Debugf("%s\n", format)
 	l.DebugCount += 1
 }
 
-func (l Crglogging) Debug(format ...interface{}) {
+func (l *Crglogging) Debug(format ...interface{}) {
 	l.logger.Debug(format)
 	l.DebugCount += 1
 }
--- crglogging/warn.go
+++ crglogging/warn.go
@@ -4,29 +4,29 @@ type Warnf func(string, ...interface{})
 type Warnln func(string)
 type Warn func(string)
 
-func (l Crglogging) Warnf(format string, args ...interface{}) {
+func (l *Crglogging) Warnf(format string, args ...interface{}) {
 	l.logger.Warningf(format, args...)
 	l.WarnCount += 1
 }
 
-func (l Crglogging) Warnln(format string) {
+func (l *Crglogging) Warnln(format string) {
 	l.logger.Warningf("%s\n", format)
 	l.WarnCount += 1
 }
 
-func (l Crglogging) Warn(format ...interface{}) {
+func (l *Crglogging) Warn(format ...interface{}) {
 	l.logger.Warning(format)
 	l.WarnCount += 1
 }
 
-func (l Crglogging) Warningf(format string, args ...interface{}) {
+func (l *Crglogging) Warningf(format string, args ...interface{}) {
 	l.Warnf(format, args...)
 }
 
-func (l Crglogging) Warningln(format string) {
+func (l *Crglogging) Warningln(format string) {
 	l.Warnln(format)
 }
 
-func (l Crglogging) Warning(format ...interface{}) {
+func (l *Crglogging) Warning(format ...interface{}) {
 	l.Warn(format)
 }
