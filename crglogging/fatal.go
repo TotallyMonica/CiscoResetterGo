56692930a3e79b7e967ad97d8667f731b7e3ae71
mhanson <mrhanson1s@semo.edu>
2025-02-10 16:06:36 -0600

    Refator logging to avoid collisions

--- crglogging/fatal.go
+++ crglogging/fatal.go
@@ -5,16 +5,16 @@ type Fatalln func(string)
 type Fatal func(string)
 
 func (l Crglogging) Fatalf(format string, args ...interface{}) {
-	Logger.Fatalf(format, args...)
+	l.logger.Fatalf(format, args...)
 	l.FatalCount += 1
 }
 
-func (l Crglogging) Fatalln(format string) {
-	Logger.Fatalf("%s\n", format)
+func (l Crglogging) Fatalln(format ...interface{}) {
+	l.logger.Fatalf("%s\n", format)
 	l.FatalCount += 1
 }
 
-func (l Crglogging) Fatal(format string) {
-	Logger.Fatal(format)
+func (l Crglogging) Fatal(format ...interface{}) {
+	l.logger.Fatal(format)
 	l.FatalCount += 1
 }
