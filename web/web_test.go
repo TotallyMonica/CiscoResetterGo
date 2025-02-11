d0354318745a7ee8a13615dad62b86d7ea6d65e1
mhanson <mrhanson1s@semo.edu>
2025-02-11 16:32:52 -0600

    Finish index tests, automate possible variants

--- web/web_test.go
+++ web/web_test.go
@@ -1,19 +1,94 @@
 package web
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
 	"os"
 	"testing"
+	"time"
 )
+
+type parameters struct {
+	proto  string
+	host   string
+	port   int
+	path   string
+	method string
+}
+
+type testParams struct {
+	name string
+	args parameters
+	want int
+}
+
+func buildConditions(path string, allowedMethods []string) []testParams {
+	methodList := []string{"GET", "HEAD", "POST", "PUT", "PATCH", "DELETE"}
+	hostList := []string{"localhost", "127.0.0.1", "[::1]"}
+	protoList := []string{"http"}
+	portList := []int{8080}
+
+	tests := make([]testParams, 0)
+
+	for _, method := range methodList {
+		for _, host := range hostList {
+			for _, proto := range protoList {
+				for _, port := range portList {
+					added := false
+					params := parameters{
+						proto:  proto,
+						host:   host,
+						port:   port,
+						path:   path,
+						method: method,
+					}
+					for _, allowed := range allowedMethods {
+						if method == allowed {
+							tests = append(tests, testParams{
+								name: fmt.Sprintf("%s %s://%s:%d%s", method, proto, host, port, path),
+								want: http.StatusOK,
+								args: params,
+							})
+							added = true
+						}
+					}
+
+					if !added {
+						tests = append(tests, testParams{
+							name: fmt.Sprintf("%s %s://%s:%d%s", method, proto, host, port, path),
+							want: http.StatusMethodNotAllowed,
+							args: params,
+						})
+					}
+				}
+			}
+		}
+	}
+
+	return tests
+}
 
 func startWebServer() {
 	go ServeWeb()
 }
 
-func closeWebServer() error {
-	_, err := http.Get("http://127.0.0.1:8080/api/debug/shutdown/")
-	return err
+func closeWebServer() {
+	client := &http.Client{
+		Timeout: time.Second * 10,
+	}
+
+	req, err := http.NewRequest("GET", "http://localhost:8080", nil)
+	if err != nil {
+		log.Fatalf("Couldn't create request: %v", err)
+	}
+
+	resp, err := client.Do(req)
+	if err != nil && !errors.Is(err, http.ErrHandlerTimeout) {
+		log.Fatalf("Couldn't call close web server: %v", err)
+	} else if resp.StatusCode != http.StatusOK {
+		log.Fatalf("Received unexpected status code %d from web server", resp.StatusCode)
+	}
 }
 
 func TestIndex(t *testing.T) {
@@ -22,43 +97,24 @@ func TestIndex(t *testing.T) {
 		t.FailNow()
 	}
 
+	t.Cleanup(closeWebServer)
+
 	startWebServer()
 
-	client := &http.Client{}
-
-	type args struct {
-		path   string
-		method string
-	}
-
-	tests := []struct {
-		name string
-		args args
-		want int
-	}{{
-		name: "GET /",
-		args: args{path: "/", method: "GET"},
-		want: http.StatusOK,
-	}, {
-		name: "POST /",
-		args: args{path: "/", method: "POST"},
-		want: http.StatusMethodNotAllowed,
-	}, {
-		name: "DELETE /",
-		args: args{path: "/", method: "DELETE"},
-		want: http.StatusMethodNotAllowed,
-	}, {
-		name: "PUT /",
-		args: args{path: "/", method: "PUT"},
-		want: http.StatusMethodNotAllowed,
-	}}
-
-	for _, tt := range tests {
+	for _, tt := range buildConditions("/", []string{"GET"}) {
+		t.Logf("Testing full path: %s %s://%s%s", tt.args.method, tt.args.proto, tt.args.host, tt.args.path)
 		t.Run(tt.name, func(t *testing.T) {
-			req, err := http.NewRequest(tt.args.method, fmt.Sprintf("http://127.0.0.1:8080/%s", tt.args.path), nil)
+			// Build client
+			client := &http.Client{
+				Timeout: time.Second * 10,
+			}
+
+			// Build out request
+			req, err := http.NewRequest(tt.args.method, fmt.Sprintf("%s://%s:%d%s", tt.args.proto, tt.args.host, tt.args.port, tt.args.path), nil)
 			if err != nil {
 				t.Errorf("Test %s failed while creating request with error: %s", tt.name, err)
 			}
+
 			resp, err := client.Do(req)
 			if err != nil {
 				t.Errorf("Test %s failed with error: %s", tt.name, err)
@@ -68,9 +124,5 @@ func TestIndex(t *testing.T) {
 		})
 	}
 
-	err := closeWebServer()
-
-	if err != nil {
-		t.Errorf("Could not shut down web server, error: %s\n", err.Error())
-	}
+	t.Logf("Finishing index tests...")
 }
